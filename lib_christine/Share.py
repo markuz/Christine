fea94b11592761ac6abdffd2b0deba3051a08e69
demrit <demrit@gnu.org>
2007-03-07 07:05:38 +0000

    Share class added to do share files job, glade template and images

---
+++ lib_christine/Share.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the Christine project
+#
+# Copyright (c) 2006-2007 Marco Antonio Islas Cruz
+#
+# Christine is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Christine is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# @category  libchristine
+# @package   Share
+# @author    Miguel Vazquez Gocobachi <demrit@gnu.org>
+# @copyright 2007 Christine Development Group
+# @license   http://www.gnu.org/licenses/gpl.txt
+import os
+import gtk.glade
+import pattern.Singleton
+
+#
+# Share class manager for images, glade 
+# templates and more files
+#
+# @author Miguel Vazquez Gocobachi <demrit@gnu.org>
+# @since 0.3
+class Share(pattern.Singleton.Singleton):
+	"""
+	Share class manager for images, glade 
+	templates and more files
+	"""
+	#
+	# Directory where we have template files
+	#
+	# @var string
+	__PathTemplate = None
+	
+	#
+	# Directory where we have images
+	#
+	# @var string
+	__PathPixmap = None
+	
+	#
+	# Constructor
+	#
+	# @param  string sharePath Path to share files
+	# @return void
+	def __init__(self, sharePath = '/usr/local/share/christine'):
+		"""
+		Constructor
+		"""
+		pattern.Singleton.Singleton.__init__(self, 'Share')
+		self.__PathTemplate = sharePath + '/gui/'
+		self.__PathPixmap   = sharePath + '/gui/pixmap/'
+	
+	#
+	# Gets glade template
+	#
+	# @param  string file The file name without extension
+	# @return string or None
+	def getTemplate(self, file = None):
+		"""
+		Gets glade template
+		"""
+		if ((file is not None) or (file != '')):
+			file + '.glade'
+			if (os.path.isfile(self.__PathTemplate + file)):
+				return gtk.glade.XML(self.__PathTemplate + file, None, None)
+		return None
+	
+	#
+	# Gets image as path string
+	#
+	# @param  string name The image name without extension
+	# @return string or None
+	def getImage(self, name):
+		"""
+		Gets image as path string
+		"""
+		if ((file is not None) or (file != '')):
+			if (os.path.isfile(self.__PathPixmap + file + '.png')):
+				return self.__PathTemplate + file + '.png'
+			elif (os.path.isfile(self.__PathPixmap + file + '.svg')):
+				return self.__PathTemplate + file + '.svg'
+		return None
