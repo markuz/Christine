eab773704b3f7c898892c3b2c19c85ac628739d1
Marco Antonio Islas Cruz <markuz@islascruz.org>
2007-04-17 21:41:23 +0000

    improved a little bit

--- libchristine/Library.py
+++ libchristine/Library.py
@@ -300,7 +300,7 @@ class library(GtkMisc,gtk.DrawingArea):
 						SEARCH,",".join([name,album,artist]),
 						PLAY_COUNT,0,
 						GENRE,genre)
-			time.sleep(0.009)
+			time.sleep(0.09)
 			#gobject.timeout_add(150,self.emit_signal,"tags-found")
 			self.emit_signal("tags-found")
 		if t == gst.MESSAGE_ERROR:
