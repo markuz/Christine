a6bf8b24417b4d9719e84af36fbe24f6f6a61341
Marco Antonio Islas Cruz <markuz@islascruz.org>
2007-07-03 14:46:43 +0000

    First Upload

---
+++ christine.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import os
+import sys
+sys.path.insert(0,os.getcwd())
+elements = locals()
+#lista = [k for k in elements['arguments'] if type(k) == str]
+#sys.argv = lista
+from libchristine.libs_christine import sanity
+sanity()
+from libchristine.Christine import *
+a = Christine()
+if len(sys.argv) > 1 and not "--devel" in sys.argv:
+	for i in sys.argv[1:]:
+		if os.path.isfile(i):
+			a.Queue.add(i,prepend=True)
+	a.play()
+a.runGtk()
+
