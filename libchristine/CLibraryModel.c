378fb6947af3d3bfb92c32c8626adc32c20e5bc5
Marco Antonio Islas Cruz <markuz@islascruz.org>
2009-07-13 08:42:31 +0000

    First upload, this will containg C methods for the christineModel in the hope it becomes a little bit faster

---
+++ libchristine/CLibraryModel.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <Python.h>
+#include <import.h>
+#include <graminit.h>
+#include "structmember.h"
+
+
+static PyObject*
+init(PyObject *self){
+	Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyObject *
+on_get_iter(PyObject *self, PyObject *args)
+{
+	static PyObject *rowref;
+	static PyObject *result;
+	static PyObject *selfobj;
+	static PyObject *data;
+	//static PyObject *index;
+	int index;
+
+    if (!PyArg_ParseTuple(args, "OO", &selfobj, &rowref))
+        return NULL;
+    data = PyObject_GetAttrString(selfobj, "data");
+    if (!data)
+    	return Py_None;
+    PyArg_ParseTuple(rowref, "i", &index);
+    if (index <= PyList_Size(data) -1){
+    	result = PyList_GetItem(data, index);
+    }
+    else{
+    	return Py_None;
+    }
+	if (!result)
+    	return Py_None;
+	//PyObject_Print(	result, stderr, Py_PRINT_RAW);
+    return Py_BuildValue("O", result);
+}
+
+static PyMethodDef CLibraryModelMethods[] = {
+	{"__init__", init, METH_VARARGS,
+			 "doc string"},
+    {"on_get_iter",  on_get_iter, METH_VARARGS,
+     "Extend.. something."},
+    {NULL, NULL, 0, NULL}
+};
+
+
+static PyMethodDef ModuleMethods[] = { {NULL} };
+
+#ifdef __cplusplus
+extern "C"
+#endif
+PyMODINIT_FUNC
+initCLibraryModel(void)
+{
+
+	PyMethodDef *def;
+	PyImport_AddModule("CLibraryModel");
+    //(void) Py_InitModule("CLibraryModel", CLibraryModelMethods);
+    PyObject *module = Py_InitModule("CLibraryModel", ModuleMethods);
+    PyObject *moduleDict = PyModule_GetDict(module);
+    PyObject *classDict = PyDict_New();
+    PyObject *className = PyString_FromString("CLibraryModel");
+    PyObject *fooClass = PyClass_New(NULL, classDict, className);
+
+    PyDict_SetItemString(moduleDict, "CLibraryModel", fooClass);
+    Py_DECREF(classDict);
+    Py_DECREF(className);
+    Py_DECREF(fooClass);
+
+    /* add methods to class */
+    for (def = CLibraryModelMethods; def->ml_name != NULL; def++) {
+    	PyObject *func = PyCFunction_New(def, NULL);
+    	PyObject *method = PyMethod_New(func, NULL, fooClass);
+    	PyDict_SetItemString(classDict, def->ml_name, method);
+    	Py_DECREF(func);
+    	Py_DECREF(method);
+     }
+
+}
+
+
+int
+main(int argc, char *argv[])
+{
+    /* Pass argv[0] to the Python interpreter */
+    Py_SetProgramName(argv[0]);
+
+    /* Initialize the Python interpreter.  Required. */
+    Py_Initialize();
+
+    /* Add a static module */
+    initCLibraryModel();
+    return 0;
+}
